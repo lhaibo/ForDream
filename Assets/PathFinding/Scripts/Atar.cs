90d29002db1e70399b640bc8093ac518d05d6826
haibo_company <haibofly@outlook.com>
2021-11-07 19:26:49 +0800

    add grid map

--- Assets/PathFinding/Scripts/Atar.cs
+++ Assets/PathFinding/Scripts/Atar.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Atar : MonoBehaviour
+public class Atar
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Execute()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PathFinding()
     {
-        
+
     }
 }
